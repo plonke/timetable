c8fc94ae482b7fd808211573c1f3a13c0243644b
pawlo <st.pawlo@gamil.com>
2017-09-09 01:57:19 +0300

    make it better

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
+const { Pool, Client } = require('pg')
 const TelegramBot = require('node-telegram-bot-api');
-
-// replace the value below with the Telegram token you receive from @BotFather
 const token = '255704702:AAGm_IG22M0tBeWp8JfhYKxj0EJFe18-IQQ';
-
-// Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
 bot.deleteWebHook()
 
-bot.onText(/\/echo (.+)/, (msg, match) => {
+const pool = new Pool({
+    user: 'dyecvowfqxalaq',
+    host: 'ec2-54-247-120-169.eu-west-1.compute.amazonaws.com',
+    database: 'd8blu11lv08bo6',
+    password: '1ea81da38eb5aa66c3782e87afadf44f0ea65204b1bf90be7a07a82178f4dcab',
+    port: 5432,
+})
+
+pool.query('SELECT NOW()', (err, res) => {
+    console.log(err, res)
+    pool.end()
+})
+
+bot.onText(/\/start (.+)/, (msg, match) => {
     const chatId = msg.chat.id;
-    const resp = match[1];
-    bot.sendMessage(chatId, resp);
 });
