3c714b28600d1ff01771b219829e69afa248b17d
pawlo <st.pawlo@gamil.com>
2017-09-10 20:57:38 +0300

    v1.0.0

--- getKeyboard.js
+++ getKeyboard.js
@@ -1,79 +1,80 @@
 module.exports = (data) => {
-    return {
-        inline_keyboard: [
-            getDayButtons(data),
-            getCommonButtons(data),
-            getWeekButtons(data)
-        ]
-    }
-}
 
-function getDayButtons(data) {
-    const texts = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт']
-    let btns = []
-    for (let i = 0; i < texts.length; i++) {
-        let text = ''
-        if (data.day === i + 1) {
-            text = '☑️ ' + texts[i]
-        } else {
-            text = texts[i]
+    function getDayButtons() {
+        const texts = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт']
+        let btns = []
+        for (let i = 0; i < texts.length; i++) {
+            let text = ''
+            if (data.day === i + 1) {
+                text = '☑️ ' + texts[i]
+            } else {
+                text = texts[i]
+            }
+            btns.push({
+                text: text,
+                callback_data: JSON.stringify({
+                    today: false,
+                    tomorrow: false,
+                    week: data.week,
+                    day: i + 1
+                })
+            })
         }
-        btns.push({
-            text: text,
+        return btns
+    }
+
+    function getCommonButtons() {
+        let today = false
+        let tomorrow = false
+        if (currentWeek === data.week) {
+            if (moment().day() === data.day) {
+                today = true
+            }
+            if (moment().day() + 1 === data.day) {
+                tomorrow = true
+            }
+        }
+
+        return [{
+            text: today ? '☑️ Сегодня' : 'Сегодня',
+            callback_data: JSON.stringify({
+                today: true,
+                tomorrow: false
+            })
+        }, {
+            text: tomorrow ? '☑️ Завтра' : 'Завтра',
+            callback_data: JSON.stringify({
+                today: false,
+                tomorrow: true
+            })
+        }]
+    }
+
+    function getWeekButtons() {
+        return [{
+            text: data.week === 2 ? '☑️ Четная' : 'Четная',
             callback_data: JSON.stringify({
                 today: false,
                 tomorrow: false,
-                week: data.week,
-                day: i + 1
+                week: 2,
+                day: data.day
             })
-        })
-    }
-    return btns
-}
-
-function getCommonButtons(data) {
-    let today = false
-    let tomorrow = false
-    if (currentWeek === data.week) {
-        if (moment().day() === data.day) {
-            today = true
-        }
-        if (moment().day() + 1 === data.day) {
-            tomorrow = true
-        }
+        }, {
+            text: data.week === 1 ? '☑️ Нечетная' : 'Нечетная',
+            callback_data: JSON.stringify({
+                today: false,
+                tomorrow: false,
+                week: 1,
+                day: data.day
+            })
+        }]
     }
 
-    return [{
-        text: today ? '☑️ Сегодня' : 'Сегодня',
-        callback_data: JSON.stringify({
-            today: true,
-            tomorrow: false
-        })
-    }, {
-        text: tomorrow ? '☑️ Завтра' : 'Завтра',
-        callback_data: JSON.stringify({
-            today: false,
-            tomorrow: true
-        })
-    }]
-}
-
-function getWeekButtons(data) {
-    return [{
-        text: data.week === 2 ? '☑️ Четная' : 'Четная',
-        callback_data: JSON.stringify({
-            today: false,
-            tomorrow: false,
-            week: 2,
-            day: data.day
-        })
-    }, {
-        text: data.week === 1 ? '☑️ Нечетная' : 'Нечетная',
-        callback_data: JSON.stringify({
-            today: false,
-            tomorrow: false,
-            week: 1,
-            day: data.day
-        })
-    }]
+    return {
+        inline_keyboard: [
+            getDayButtons(),
+            getCommonButtons(),
+            getWeekButtons()
+        ]
+    }
 }
